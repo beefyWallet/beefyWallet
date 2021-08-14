325daff53a86696b926f7441c21da93837228075
Ayman Naif <ayman_naif@hotmail.com>
2021-08-14 21:16:47 +0300

    add Login, signUp and Advice UI

--- beefywallet/pages/app/Advice.js
+++ beefywallet/pages/app/Advice.js
@@ -1,23 +1,117 @@
 import BeefyWalletAdmin from '../../src/context/BeefyWalletAdmin'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import clsx from 'clsx'
-import useStyles from '../../useStyle'
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import data from '../../data';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export default function Advice() {
-  const classes = useStyles()
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <React.Fragment>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.initiallyHeight}>
-            <h1 className="text-2xl">Advice</h1>
-          </Paper>
+
+    <>
+      <Grid  container spacing={1}>
+      {data[0].money_sources.map(item =>((
+        <Grid item xs={4} spacing={2}>
+          <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            A
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Advice one"
+        subheader="August 14, 2021"
+      />
+      <CardMedia
+        className={classes.media}
+        image="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80"
+        title="Advice One"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          short discribtion for the Advice
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton> */}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>
+            More details about the advice ..
+          </Typography>
+        </CardContent>
+      </Collapse>
+        </Card>
         </Grid>
+        )))}
+        
       </Grid>
-    </React.Fragment>
-  )
+  </>
+  );
 }
 Advice.Layout = BeefyWalletAdmin
