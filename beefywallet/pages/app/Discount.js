4b5608a76cafe074e3713e2825919a6caa25fc0f
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-08-14 21:24:06 +0300

    doing Discount page

--- beefywallet/pages/app/Discount.js
+++ beefywallet/pages/app/Discount.js
@@ -1,23 +1,75 @@
 import BeefyWalletAdmin from '../../src/context/BeefyWalletAdmin'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import clsx from 'clsx'
-import useStyles from '../../useStyle'
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import data from '../../data';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
 
 export default function Discount() {
-  const classes = useStyles()
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <React.Fragment>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.initiallyHeight}>
-            <h1 className="text-2xl">Discount</h1>
-          </Paper>
+
+    <>
+      <Grid  container spacing={3}>
+      {data[0].money_sources.map(item =>((
+        <Grid item xs={4} spacing={2}>
+          <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://st.depositphotos.com/2036511/2769/v/950/depositphotos_27691617-stock-illustration-red-20-percent-discount-sign.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Discount Title
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Vendor 
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Date 
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Discount Type 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Discount Info .....................
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        {/* <Button size="small" color="primary">
+          Visit
+        </Button> */}
+      </CardActions>
+    </Card>
         </Grid>
+        )))}
+        
       </Grid>
-    </React.Fragment>
-  )
+  </>
+  );
 }
 Discount.Layout = BeefyWalletAdmin
