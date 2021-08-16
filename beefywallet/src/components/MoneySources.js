71937e29568ef7eec202b6e930637fe9621aff89
amarh-ayman <amarhbishtawi@gmail.com>
2021-08-16 14:42:07 +0300

    *

--- beefywallet/src/components/MoneySources.js
+++ beefywallet/src/components/MoneySources.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import { Grid } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import { Grid } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   loading: {
     marginTop: theme.spacing(2),
     // marginRight: theme.spacing(2),
     fontSize: 16,
   },
   root: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: 'flex',
+    flexWrap: 'wrap',
     flexGrow: 1,
-    "& > *": {
+    '& > *': {
       margin: theme.spacing(2),
       width: theme.spacing(20),
       height: theme.spacing(10),
@@ -29,8 +29,8 @@ const useStyles = makeStyles((theme) => ({
     left: 20,
   },
   container: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   formControl: {
     margin: theme.spacing(2),
@@ -38,9 +38,9 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 120,
   },
   drawerPaper: {
-    display: "flex",
-    flexWrap: "wrap",
-    "& > *": {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
       margin: theme.spacing(1),
       width: theme.spacing(16),
       height: theme.spacing(4),
@@ -48,24 +48,24 @@ const useStyles = makeStyles((theme) => ({
   },
   textField: {
     margin: theme.spacing(1),
-    width: "25ch",
+    width: '25ch',
   },
-}));
+}))
 
 function camelCase(word) {
-  return word[0].toUpperCase() + word.substring(1);
+  return word[0].toUpperCase() + word.substring(1)
 }
 
 // ###########################################
 export default function MoneySources({ moneySourceData }) {
-  // console.log(data3);
-  const classes = useStyles();
+  // console.log(moneySourceData)
+  const classes = useStyles()
 
   return (
     <React.Fragment>
       <Grid container spacing={3} className={classes.initiallyTopMargin}>
         <div className={classes.root}>
-          {moneySourceData.map((moneySource) => (
+          {moneySourceData.map(moneySource => (
             <Grid item key={moneySource.id} xs={5} md={2} lg={2}>
               <Paper className={classes.drawerPaper}>
                 <Typography component="p" variant="h6">
@@ -80,5 +80,5 @@ export default function MoneySources({ moneySourceData }) {
         </div>
       </Grid>
     </React.Fragment>
-  );
+  )
 }
--- beefywallet/src/components/Reports.js
+++ beefywallet/src/components/Reports.js
@@ -1,26 +1,36 @@
-import React from "react";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Title from "./Title";
-import data from "../../data";
+import React from 'react'
+import Link from '@material-ui/core/Link'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Title from './Title'
+import data from '../../data'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
 
 function preventDefault(event) {
-  event.preventDefault();
+  event.preventDefault()
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   seeMore: {
     marginTop: theme.spacing(3),
   },
-}));
+}))
+
+function getDate(date) {
+  const newDate = new Date(date)
+  return `${newDate.getFullYear()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getDate()}`
+}
 
 export default function Reports({ transactionsData }) {
-  const classes = useStyles();
+  console.log(transactionsData)
+  const classes = useStyles()
   return (
     <React.Fragment>
       <Title>Recent Reports</Title>
@@ -30,31 +40,27 @@ export default function Reports({ transactionsData }) {
             <TableCell>Date</TableCell>
             <TableCell>Source</TableCell>
             <TableCell>Tranaction</TableCell>
-            {/* <TableCell>incomes</TableCell> */}
+            <TableCell>Category</TableCell>
             <TableCell>Amount</TableCell>
             <TableCell>Note</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {transactionsData.map((row) => (
+          {transactionsData.map(row => (
             <TableRow key={row.id}>
-              <TableCell>{row.creation_date}</TableCell>
+              <TableCell>{getDate(row.creation_date)}</TableCell>
               <TableCell>{row.money_source.name}</TableCell>
               <TableCell>
-                {/* {row.expenses.reduce(
-                  (sum, expense) => sum + parseInt(expense.value),
-                  0
-                )} */}
+                {row.transaction_type == 'Income' ? (
+                  <ArrowDownwardIcon />
+                ) : (
+                  <ArrowUpwardIcon />
+                )}
+
                 {row.transaction_type}
               </TableCell>
-
-              <TableCell>
-                {/* {row.incomes.reduce(
-                  (sum, income) => sum + parseInt(income.value),
-                  0
-                )} */}
-                {row.value} &nbsp; JOD
-              </TableCell>
+              <TableCell>{row.category}</TableCell>
+              <TableCell>{row.value} &nbsp; JOD</TableCell>
               <TableCell>{row.note}</TableCell>
             </TableRow>
           ))}
@@ -66,5 +72,5 @@ export default function Reports({ transactionsData }) {
         </Link>
       </div>
     </React.Fragment>
-  );
+  )
 }
