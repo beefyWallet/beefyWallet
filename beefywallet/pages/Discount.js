38092f0b9be261895eeaf4768f495e982637f813
Ayman Naif <ayman_naif@hotmail.com>
2021-08-16 23:07:53 +0300

    Redesign the Discount page and design the logo, and add Welcome page

--- beefywallet/pages/Discount.js
+++ beefywallet/pages/Discount.js
@@ -3,22 +3,11 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
-import clsx from "clsx";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import data from "../data";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
@@ -80,7 +69,7 @@ export default function Advice() {
 
   return (
     <>
-      <Grid container spacing={1}>
+      <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.initiallyHeight}>
             <h1 className="text-2xl">Discount's</h1>
@@ -88,62 +77,45 @@ export default function Advice() {
         </Grid>
 
         {adsData.map((item) => (
-          <Grid item xs={4} spacing={2}>
-            <Card className={classes.root}>
-              <CardHeader
-                // avatar={
-                //   <Avatar aria-label="recipe" className={classes.avatar}>
-                //     A
-                //   </Avatar>
-                // }
-                // action={
-                //   <IconButton aria-label="settings">
-                //     <MoreVertIcon />
-                //   </IconButton>
-                // }
-                title={item.title}
-                subheader={item.creation_date}
-              />
-              <CardMedia
+          
+          <Grid item xs={12}  >
+         <Paper>
+
+          
+            <Grid container spacing={0} >
+            <Grid item xs={4} >
+              <CardMedia className={classes.root}
                 component="img"
                 alt="Discount"
                 height="100"
-                // className={classes.mediaDiscount}
                 image={item.image}
                 title="Discount"
               />
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
+              </Grid>
+              <Grid item xs={8}>
+                <CardContent>
+                <Typography  variant="h5" gutterBottom>
+                  {item.title}
+                </Typography>
+                <Typography color="textSecondary" variant="subtitle1" gutterBottom>
+                 time: {item.creation_date}
+                </Typography>
+            
+                  <Typography variant="body2" color="textSecondary" component="p">
                   {item.text}
                 </Typography>
               </CardContent>
-              <CardActions disableSpacing>
-                {/* <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton> */}
-                {/* <IconButton
-                  className={clsx(classes.expand, {
-                    [classes.expandOpen]: expanded,
-                  })}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </IconButton> */}
-              </CardActions>
               <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
                   <Typography>More details about the Discount ..</Typography>
                 </CardContent>
-              </Collapse>
-            </Card>
+                </Collapse>
+                </Grid>
+            </Grid></Paper>
           </Grid>
         ))}
       </Grid>
+
     </>
   );
 }
