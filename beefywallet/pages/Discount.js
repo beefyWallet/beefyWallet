c59492e10f67aa2e731eb8376a000eaa724626d3
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-08-17 04:06:48 +0300

    merge aymans work for discount page

--- beefywallet/pages/Discount.js
+++ beefywallet/pages/Discount.js
@@ -1,6 +1,6 @@
 // import BeefyWalletAdmin from '../../src/context/BeefyWalletAdmin'
 import React from "react";
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -8,19 +8,15 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import data from "../data";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { ApiDataContext } from "../src/context/apiData";
 const useStyles = makeStyles((theme) => ({
+  marginBetween: {
+    marginBottom: 20,
+  },
   root: {
     maxWidth: 345,
   },
@@ -69,59 +65,54 @@ export default function Advice() {
         </Grid>
 
         {adsData.map((item) => (
-          <Grid item xs={4} spacing={2}>
-            <Card className={classes.root}>
-              <CardHeader
-                // avatar={
-                //   <Avatar aria-label="recipe" className={classes.avatar}>
-                //     A
-                //   </Avatar>
-                // }
-                // action={
-                //   <IconButton aria-label="settings">
-                //     <MoreVertIcon />
-                //   </IconButton>
-                // }
-                title={item.title}
-                subheader={item.creation_date}
-              />
-              <CardMedia
-                component="img"
-                alt="Discount"
-                height="100"
-                // className={classes.mediaDiscount}
-                image={item.image}
-                title="Discount"
-              />
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {item.text}
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                {/* <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton> */}
-                {/* <IconButton
-                  className={clsx(classes.expand, {
-                    [classes.expandOpen]: expanded,
-                  })}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </IconButton> */}
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography>More details about the Discount ..</Typography>
-                </CardContent>
-              </Collapse>
-            </Card>
+          <Grid item xs={12} className={classes.marginBetween}>
+            <Paper>
+              <Grid container spacing={0}>
+                <Grid item xs={4}>
+                  <CardMedia
+                    className={classes.root}
+                    component="img"
+                    alt="Discount"
+                    height="100"
+                    image={item.image}
+                    title="Discount"
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <CardContent>
+                    <Typography variant="h5" gutterBottom>
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Time: {item.creation_date}
+                    </Typography>
+
+                    <Typography variant="body1" component="p">
+                      {item.text}
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      <br /> Type: {item.ad_type}
+                      <br /> Vendor: {item.vendor}
+                    </Typography>
+                  </CardContent>
+                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                      <Typography>
+                        More details about the Discount ..
+                      </Typography>
+                    </CardContent>
+                  </Collapse>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
         ))}
       </Grid>
