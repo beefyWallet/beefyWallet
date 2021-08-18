bad30db37b428d6f0352b4866e46814dce48c1b3
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-08-18 13:57:53 +0300

    Add amarah work

--- beefywallet/pages/currency-converter.js
+++ beefywallet/pages/currency-converter.js
@@ -1,13 +1,14 @@
 import React, { useState, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 import PageHeader from "../src/components/PageHeader";
-
-const useStyles = makeStyles({
+import SyncAltIcon from "@material-ui/icons/SyncAlt";
+import clsx from "clsx";
+const useStyles = makeStyles((theme) => ({
   totalContext: {
     flex: 1,
   },
@@ -20,7 +21,20 @@ const useStyles = makeStyles({
     padding: 12,
     borderRadius: 2,
   },
-});
+  marginbottom: {
+    marginBottom: theme.spacing(3),
+  },
+  marginleft: {
+    marginLeft: theme.spacing(6),
+  },
+  icon: {
+    marginLeft: theme.spacing(5),
+    marginTop: theme.spacing(1),
+  },
+  width: {
+    width: theme.spacing(43),
+  },
+}));
 
 export default function CurrentConverter() {
   const [first, setFirst] = useState("");
@@ -48,49 +62,61 @@ export default function CurrentConverter() {
             //   className={classes.initiallyTopMargin}
             noValidate
             autoComplete="off"
+            className={classes.root}
           >
-            <Grid container>
-              <Grid item md={12}>
-                <TextField
-                  id="amount"
-                  label="Amount"
-                  variant="outlined"
-                  name="Amount"
-                  fullWidth
-                  onChange={(e) => setAmount(e.target.value.toUpperCase())}
-                />
-                <TextField
-                  id="first"
-                  label="USD EUR JPY GBP AUD JOD ..."
-                  variant="outlined"
-                  name="first"
-                  fullWidth
-                  onChange={(e) => setFirst(e.target.value.toUpperCase())}
-                />
-                <TextField
-                  id="second"
-                  label="USD EUR JPY GBP AUD JOD ..."
-                  variant="outlined"
-                  name="second"
-                  fullWidth
-                  onChange={(e) => setSecond(e.target.value)}
-                />
-              </Grid>
-              <Grid item md={12}>
-                <Button
-                  type="submit"
-                  variant="outlined"
-                  fullWidth
-                  color="primary"
-                  onClick={handleSubmit}
-                  style={{
-                    marginTop: 10,
-                  }}
-                >
-                  Convert
-                </Button>
-              </Grid>
+            {/* <Grid container> */}
+            <Grid item md={12}>
+              <TextField
+                id="amount"
+                label="Amount"
+                variant="outlined"
+                name="Amount"
+                fullWidth
+                className={classes.marginbottom}
+                onChange={(e) => setAmount(e.target.value.toUpperCase())}
+              />
+              <TextField
+                id="first"
+                label="Currency 1"
+                helperText="USD EUR JPY GBP AUD JOD ..."
+                variant="outlined"
+                name="first"
+                // fullWidth
+                className={classes.width}
+                onChange={(e) => setFirst(e.target.value.toUpperCase())}
+              />
+              <SyncAltIcon
+                style={{ fontSize: 43 }}
+                color="primary"
+                className={classes.icon}
+              />
+              <TextField
+                id="second"
+                label="Currency 2"
+                helperText="USD EUR JPY GBP AUD JOD ..."
+                variant="outlined"
+                name="second"
+                className={clsx(classes.marginleft, classes.width)}
+                // fullWidth
+                onChange={(e) => setSecond(e.target.value)}
+              />
             </Grid>
+            <Grid item md={12}>
+              <Button
+                type="submit"
+                variant="outlined"
+                fullWidth
+                color="primary"
+                onClick={handleSubmit}
+                style={{
+                  marginTop: 10,
+                  height: 70,
+                }}
+              >
+                Convert
+              </Button>
+            </Grid>
+            {/* </Grid> */}
           </form>
         </Grid>
         <Grid item md={3} className={classes.output}>
